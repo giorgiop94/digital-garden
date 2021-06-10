35b58e5050dca3333278e677ba20a047b2b4083d
Daniel Souza <danisztls@gmail.com>
2021-06-10 15:20:09 -0300

    Extend highlight to pages

--- _plugins/obsidian.rb
+++ _plugins/obsidian.rb
@@ -3,3 +3,9 @@ Jekyll::Hooks.register :notes, :pre_render do |post, payload|
     post.content = post.content.gsub(/==(.+?)==/, '<mark>\1</mark>')
   end
 end
+
+Jekyll::Hooks.register :pages, :pre_render do |post, payload|
+  if payload['site']['markdown_ext'].include? post.extname.tr('.', '')
+    post.content = post.content.gsub(/==(.+?)==/, '<mark>\1</mark>')
+  end
+end
