e3715ee095200a22dd6c951d0a00f67df1fcfaba
Daniel Souza <danisztls@gmail.com>
2021-06-10 15:19:36 -0300

    Refactor obsidian.rb

--- _plugins/obsidian.rb
+++ _plugins/obsidian.rb
@@ -1,8 +1,5 @@
 Jekyll::Hooks.register :notes, :pre_render do |post, payload|
-  docExt = post.extname.tr('.', '')
-  # only process if we deal with a markdown file
-  if payload['site']['markdown_ext'].include? docExt
-    newContent = post.content.gsub(/==(.+?)==/, '<mark>\1</mark>')
-    post.content = newContent
+  if payload['site']['markdown_ext'].include? post.extname.tr('.', '')
+    post.content = post.content.gsub(/==(.+?)==/, '<mark>\1</mark>')
   end
 end
